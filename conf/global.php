231498be590901fe1002a27d5fa060e26e6ee0e3
zhangjianqiu <179470917@qq.com>
2019-05-14 10:53:55 +0800

    1

--- conf/global.php
+++ conf/global.php
@@ -1,23 +1,25 @@
-<?php
-/* error_reporting(E_ALL);
-echo "<pre />";
-var_dump($_SERVER);
-echo $_SERVER['REQUEST_SCHEME']."<br/>";
+﻿<?php
+error_reporting(E_ALL);
+
+/*echo "<pre />";
+var_dump($_SERVER);*/
+/* echo $_SERVER['REQUEST_SCHEME']."<br/>";
 echo $_SERVER['REQUEST_SCHEME']."<br/>";
 echo $_SERVER['SERVER_PROTOCOL']."<br/>";
 echo $_SERVER['HTTP_HOST']."<br/>";
 echo $_SERVER['SERVER_PORT']."<br/>";
 echo $_SERVER['SERVER_PORT']."<br/>";
 echo $_SERVER['SCRIPT_NAME']."<br/>"; */
-$http_s=isset($_SERVER['REQUEST_SCHEME'])?$_SERVER['REQUEST_SCHEME']:$_SERVER['SERVER_PROTOCOL'];
-//$str=$http_s.'://'.$_SERVER['HTTP_HOST']..dirname($_SERVER['SCRIPT_NAME']).'/uploads/';
+$http_s=/*isset($_SERVER['REQUEST_SCHEME'])?$_SERVER['REQUEST_SCHEME']: */$_SERVER['SERVER_PROTOCOL'];
 $port_s=($_SERVER['SERVER_PORT']=='80'?'':':'.$_SERVER['SERVER_PORT']);
 /* echo $http_s."--".$port_s; */
-$myMakeUrl=$http_s."://".$_SERVER['HTTP_HOST'].$port_s;
-/* echo $myMakeUrl; */
+$myMakeUrl="https://".$_SERVER['HTTP_HOST'];
+//$myMakeUrl="http://".$_SERVER['HTTP_HOST'].$_SERVER['SERVER_PORT'])
+ //echo $myMakeUrl; exit;
 return new \Phalcon\Config([
     'base' => [
         "curlapi"=>"https://www.china-flag-makers.com/",
+        "curl_api"=>"http://cfmfactory.com/",
         'database' => [
             'default' => [
                 'adapter' => 'Mysql',
@@ -34,16 +36,22 @@ return new \Phalcon\Config([
                 //本地服务器
                 'dbname' => 'ctimg',
                 'prefix' => 'dc_',
-                'host' => '139.199.57.82',
+                'host' => '127.0.0.1',
                 'username' => 'root',
-                'password' => 'lamp1234',
+                'password' => 'DRsXT5ZJ6Oi55LPQ',
                 //测试服务器
-                
-                /* 'dbname' => 'bannerpr_cfm',
+                /*'dbname' => 'bannerpr_cfm',
                 'prefix' => 'dc_',
                 'host' => '101.132.112.217',
                 'username' => 'root',
-                'password' => 'd12f41c162cb158c', */
+                'password' => 'd12f41c162cb158c',*/
+
+                //正式服务器
+                /*'prefix' => 'dc_',
+                'host' => 'rm-0xi7dx7rb62zmrv5jyo.mysql.rds.aliyuncs.com:3306',
+                'username' => 'cfm',
+                'password' => 'cfm@20181217',
+                'dbname' => 'bannerpr_cfm',*/
             ],
         ],
         'localdb' => [
@@ -65,19 +73,17 @@ return new \Phalcon\Config([
                 'host' => '127.0.0.1',
                 'username' => 'root',
                 'password' => 'root',*/
-                //测试服务器
-                
                 'dbname' => 'ctimg',
-                'prefix' => 'ct_',
+                'prefix' => 'dc_',
                 'host' => '127.0.0.1',
                 'username' => 'root',
-                'password' => 'lamp1234',
+                'password' => 'DRsXT5ZJ6Oi55LPQ',
             ],
         ],
         'env'=>'prod',
         'visualUri' => 'index.php?_url=', //配合route的handle工作,如果采用重写，这里就设置基准目录就好.同时用于生成url
         'staticUri' => '/home/', //静态文件路径，可以是绝对地址或相对地址
-        'defaultRouter' => 'admin/index/index',
+        'defaultRouter' => 'home/index/index',
         'cacheDir' => RUNTIME_DIR . '/data/',
         'log' => [
             'level' => Phalcon\Logger::ERROR,
@@ -120,7 +126,7 @@ return new \Phalcon\Config([
                //'uploadUriPrefix'=>'http://bd.ctimg.bd/uploads/',
                 'uploadDir'=> BASE_DIR.'/public/uploads/',
                 'extensions'=>'gif,jpg,jpeg,bmp,png',
-                "cfmImgUrl"=>"http://www.china-flag-makers.com/",
+                "cfmImgUrl"=>"http://www.china-flag-makers.com/public/uploads/",
             ],
             'operation'=>[],
             'auditlog'=>[],
